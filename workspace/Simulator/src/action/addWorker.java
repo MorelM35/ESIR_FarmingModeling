c96372c3a39b54ef567941fedc104036a1e04bf2
Maxime <morel.m22@gmail.com>
2015-01-22 10:47:15 +0100

    aff files Action.java and addWorker

---
+++ workspace/Simulator/src/action/addWorker.java
@@ -0,0 +1,11 @@
+package action;
+
+import activity.PeriodicActivity;
+
+public class addWorker extends Action{
+	PeriodicActivity _activity;
+	public addWorker(PeriodicActivity activity) {
+		// TODO Auto-generated constructor stub
+		_activity=activity;
+	}
+}
